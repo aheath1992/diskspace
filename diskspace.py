82f97babcd75104ca4fee271308df604bdbff866
aheath1992 <44443152+aheath1992@users.noreply.github.com>
2023-02-09 21:33:44 -0500

    Update diskspace.py
    Change import to match documentation

--- diskspace.py
+++ diskspace.py
@@ -4,7 +4,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import subprocess
-from ansible.module_utils.basic import *
+from ansible.module_utils.basic import AnsibleModule
 
 DOCUMENTATION = r'''
 ---
